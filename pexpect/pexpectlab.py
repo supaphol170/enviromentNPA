c3bfdc024b3549ae878a3cab5ad743555cd0178c
NetAcad DEVASC <changeme@example.com>
2023-03-01 08:41:16 +0000

    modify script to create loopback on R1-R3

--- pexpect/pexpectlab.py
+++ pexpect/pexpectlab.py
@@ -1,21 +1,28 @@
 import pexpect
 
+lb_ads = ['1.1.1.1', '2.2.2.2', '3.3.3.3']
+router_ads = ['172.31.104.4', '172.31.104.5', '172.31.104.6']
+
 PROMPT = '#'
 IP = "10.0.15.104"
 USERNAME = "admin"
 PASSWORD = "cisco"
-COMMAND = "show ip int b"
 
-child = pexpect.spawn("telnet " + IP)
-child.expect("Username")
-child.sendline(USERNAME)
-child.expect("Password")
-child.sendline(PASSWORD)
-child.expect(PROMPT)
-child.sendline(COMMAND)
-child.expect(PROMPT)
-result = child.before
-print(result)
-print()
-print(result.decode('UTF-8'))
-child.sendline("exit")
+for ip in range(len(router_ads)):
+    child = pexpect.spawn("telnet " + router_ads[ip])
+    child.expect("Username")
+    child.sendline(USERNAME)
+    child.expect("Password")
+    child.sendline(PASSWORD)
+    child.expect(PROMPT)
+    child.sendline('conf t')
+    child.expect(PROMPT)
+    child.sendline('int loop 0')
+    child.expect(PROMPT)
+    child.sendline('ip add ' + lb_ads[ip] + ' 255.255.255.255')
+    child.expect(PROMPT)
+    result = child.before
+    print(result)
+    print()
+    print(result.decode('UTF-8'))
+    child.sendline("exit")
