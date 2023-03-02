e1e205361b1aa4cf612e60d5d298f142e1721c50
warit@NetAcad DEVASC <warit972177747@hotmail.com>
2023-03-02 11:20:24 +0000

    modify to ssh to R0-R3 ans S0-S1

--- paramiko/paramikolab02.py
+++ paramiko/paramikolab02.py
@@ -4,7 +4,7 @@ import paramiko
 username = 'admin'
 password = 'cisco'
 
-devices_ip = ['172.31.104.4', '172.31.104.5', '172.31.104.6']
+devices_ip = ['172.31.104.1', '172.31.104.2', '172.31.104.3', '172.31.104.4', '172.31.104.5', '172.31.104.6']
 
 for ip in devices_ip:
     client = paramiko.SSHClient()
@@ -21,5 +21,5 @@ for ip in devices_ip:
 
         ssh.send("show ip int b\n")
         time.sleep(1)
-        result = ssh.recv(1000).decode('ascii')
+        result = ssh.recv(2000).decode('ascii')
         print(result)
