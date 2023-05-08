0e437e91df838009a18a3995c1f42c4d2f8995de
warit@NetAcad DEVASC <warit972177747@hotmail.com>
2023-05-08 08:49:15 +0000

    Added get_int function

--- netmiko/netmiko_hw/netmikolab.py
+++ netmiko/netmiko_hw/netmikolab.py
@@ -1,7 +1,7 @@
-from netmiko import ConnectionHandler
+from netmiko import ConnectHandler
 def get_data_from_device(device_params):
-    with ConnectionHandler(**device_params) as ssh:
-        result_shipintbr = ssh.send_command("show ip int b")
+    with ConnectHandler(**device_params) as ssh:
+        result_shipintbr = ssh.send_command("show cdp nei")
         return result_shipintbr
 
 def get_ip(device_params, intf):
@@ -12,15 +12,26 @@ def get_ip(device_params, intf):
         if words[0][0] == intf[0] and words[0][-3:] == intf[1:]:
             return words[1]
 
+def get_int(device_params):
+    int_dict = {}
+    data = get_data_from_device(device_params)
+    result = data.strip().split('\n')
+    for line in result[5:-2]:
+        words = line.split()
+        int_dict[str(words[1] + words[2])] = str(words[-2] + words[-1])
+    return int_dict
+
 if __name__ == "__main__":
     device_ip = "172.31.104.4"
     username = "admin"
     password = "cisco"
 
     device_params = {
+        "ip": device_ip,
         "device_type": "cisco_ios",
         "username": username,
         "password": password
     }
 
-    print(get_ip(device_params, "G0/0"))
+    #print(get_ip(device_params, "G0/0"))
+    print(get_int(device_params))
